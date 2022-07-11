bf1a32fd8f591b6991a2f24a088c099f0cec0254
Deepak Ugale <65403702+ugale-deepak3010@users.noreply.github.com>
2022-07-11 12:38:05 +0530

    Uploading 1
    1

---
+++ PersonProject/Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using PersonProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonProject.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            PersonModel p = new PersonModel();
+            int testError = 5 / p.Age;
+
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Deepak", LastName="Ugale" ,Age=24, IsAlive= true}) ;
+            people.Add(new PersonModel { FirstName = "John", LastName = "Smith", Age = 46, IsAlive = true });
+            people.Add(new PersonModel { FirstName = "Priya", LastName = "Patil ", Age = 34, IsAlive = true });
+            people.Add(new PersonModel { FirstName = "Anita", LastName = "Puri", Age = 19, IsAlive = true });
+
+            return View(people);
+        }
+    }
+}
